8375a7e1b3ce250d54cd563ccf5d2cb9ea119567
Dharani-2501 <suriyadhar29@gmail.com>
2026-02-05 15:23:28 +0530

    first commit

---
+++ src/test/java/com/hospital/demohos/DemohosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hospital.demohos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemohosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
